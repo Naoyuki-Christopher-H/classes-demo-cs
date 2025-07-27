1b22fa40fedc79dff93e5d1ca1fa8698f586fad6
YukiChriss <naoyuki.christopher.higaki@outlook.com>
2025-07-27 16:57:50 +0200

    Add project files.

---
+++ classes-demo-cs/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes_demo_cs
+{
+    class Program
+    {
+        // Class: 
+        // A class is a blueprint for creating objects.
+        // It defines properties and methods that the created objects will have.
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Messages messages = new Messages();
+                messages.DisplayMessage();
+                messages.Waiting();
+                messages.Goodbye();
+            }
+            catch (Exception Issue)
+            {
+                Console.WriteLine($"ERROR: {Issue.Message}");
+            }
+        }
+    }
+
+    class Messages
+    {
+        public void DisplayMessage()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public void Waiting()
+        { 
+            Console.WriteLine("I am waiting for something.");
+        }
+
+        public void Goodbye()
+        {
+            Console.WriteLine("Goodbye, World!");
+        }
+    }
+}
